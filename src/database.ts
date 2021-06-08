7e3c5deac9cf77f9b48a1edd53af5e10d05eea23
Miguel <Miguel Augusto>
2021-06-07 21:07:30 -0300

    backend

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
-const { DB_NAME, DB_PASSWORD } = process.env;
+const { DB_NAME, DB_PASSWORD, DB_HOST } = process.env;
 
 const encodedPassword = DB_PASSWORD ?? '';
 
-mongoose.connect(`mongodb+srv://${DB_NAME}:${encodedPassword}@cluster0.mhpce.mongodb.net/test`, {
+mongoose.connect(`mongodb+srv://${DB_NAME}:${encodedPassword}@${DB_HOST}/test`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
